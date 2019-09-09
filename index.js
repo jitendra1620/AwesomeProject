22d3398b763607215c8be82c9c64fb60c92ce7f3
jitendra1620 <43338745+jitendra1620@users.noreply.github.com>
2019-09-09 18:39:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-export const addRound = require('./AddRound.png'); 
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import VCManagers from './VCManagers';
+import Bananas from "./Bananas";
+import LotsOfGreetings from "./LotsOfGreetings";
+import BlinkApp from "./BlinkApp"
+import FlexDimensionsBasics from "./FlexDimensionsBasics"
+import {name as appName} from './app.json';
+
+
+import {Navigation} from 'react-native-navigation';
+import {registerScreens} from './JS/screens';
+
+registerScreens();
+// AppRegistry.registerComponent(appName, () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+    Navigation.setRoot({
+      root: {
+        component: {
+          name: 'Home'
+        }
+      },
+    });
+  });
