b2f4c69c72b6462af133c9b339d9d7eacc1020ee
jitendra1620 <43338745+jitendra1620@users.noreply.github.com>
2019-09-18 00:16:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,25 +4,33 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import VCManagers from './VCManagers';
-import Bananas from "./Bananas";
-import LotsOfGreetings from "./LotsOfGreetings";
-import BlinkApp from "./BlinkApp"
-import FlexDimensionsBasics from "./FlexDimensionsBasics"
 import {name as appName} from './app.json';
 
-
 import {Navigation} from 'react-native-navigation';
-import {registerScreens} from './JS/screens';
+import {registerScreens} from './src/screens';
 
 registerScreens();
 // AppRegistry.registerComponent(appName, () => App);
 Navigation.events().registerAppLaunchedListener(() => {
-    Navigation.setRoot({
-      root: {
-        component: {
-          name: 'Home'
-        }
+  Navigation.setRoot({
+    root: 
+    {
+      stack: {
+        id: 'AppStack',
+        children: [
+          {
+            component: {
+              name: 'Home',
+              options:{
+                topBar:{
+                  visible:false
+                }
+              }
+            },
+            
+          },
+        ],
       },
-    });
+    },
   });
+});
